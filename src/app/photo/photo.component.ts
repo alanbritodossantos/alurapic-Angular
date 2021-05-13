a32e2082529252e43e2eef67a38d6e3954ea9f23
Alan Brito <alanbritodossantos2015@gmail.com>
2021-05-12 23:36:47 -0300

    criando component

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'ap-photo',
+  templateUrl: 'photo.component.html'
+})
+
+export class PhotoComponent{
+  description = 'Leão';
+  url = 'https://exame.com/wp-content/uploads/2019/07/rei-leao.jpg';
+}
