76a0f6beb7acdfbd3dd3357759c3cec736e37663
Alan Brito <alanbritodossantos2015@gmail.com>
2021-05-15 22:05:03 -0300

    Lidando com rotas inexistentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 
 import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { AppRoutingModule } from './app.routing.module';
   imports: [
     BrowserModule,
     PhotosModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ErrorsModule
 
   ],
   providers: [],
