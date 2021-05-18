2d14997f2c7128384241fc8887bb6527792e3f61
Alan Brito <alanbritodossantos2015@gmail.com>
2021-05-18 00:26:37 -0300

    Resolvers

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,10 +4,16 @@ import { RouterModule, Routes } from "@angular/router";
 import { PhotoListComponent } from "./photos/photo-list/photo-list.component";
 import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
 import { NotFoundComponent } from "./errors/not-found/not-found.component";
+import { PhotolistResolver } from "./photos/photo-list/photo-list.resolve";
 
 
 const routes: Routes = [
-  { path: 'user/:userName', component: PhotoListComponent },
+  { path: 'user/:userName',
+   component: PhotoListComponent,
+   resolve: {
+     photos: PhotolistResolver
+   }
+  },
   { path: 'p/add', component: PhotoFormComponent },
   { path: '**', component: NotFoundComponent }
 ];
